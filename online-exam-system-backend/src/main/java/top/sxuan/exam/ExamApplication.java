f86552c6fe691beb4e593b05898b3d8e30d6b9f0
上玄 <1421148240@qq.com>
2025-12-06 12:51:15 +0800

    feat(backend): 搭建Spring Boot工程脚手架

---
+++ online-exam-system-backend/src/main/java/top/sxuan/exam/ExamApplication.java
@@ -0,0 +1,25 @@
+package top.sxuan.exam;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * SpringBoot启动类
+ *
+ */
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableAsync
+@EnableScheduling
+@EnableKnife4j
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800 * 2)
+public class ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
+}
